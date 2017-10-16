a68e9b59ff615b682bb5153516ca9f300d7b776b
jacobxq <zhihua0113@hotmail.com>
2017-10-16 11:14:14 +0800

    first commit

---
+++ src/common/js/dom.js
@@ -0,0 +1,16 @@
+export function addClass (el, className) {
+  if (!hadClass(el, className)) {
+    let newClass = el.className.split(' ')
+    newClass.push(className)
+    el.className = newClass.join(' ')
+  }
+}
+
+export function hadClass (el, className) {
+  if (el.className.indexOf(className) > 0) {
+    return true
+  }
+  return false
+  // let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  // return reg.test(el.className)
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    this's xiaoqi speaking
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.hello 
+  font-size: 50px
+  color: red
+</style>
