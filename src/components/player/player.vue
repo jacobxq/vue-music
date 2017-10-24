29de7bff76b1bc114f933ffb6237818684cb1419
jacobxq <zhihua0113@hotmail.com>
2017-10-24 21:44:30 +0800

    lyric done

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -17,15 +17,19 @@
           <h1 class="title" v-html="currentSong.name"></h1>
           <h2 class="subtitle" v-if="currentSong.singers" v-html="currentSong.singers[0]"></h2>
         </div>
-        <div class="middle">
+        <div class="middle"
+             @touchstart.prevent="middleTouchStart"
+             @touchmove.prevent="middleTouchMove"
+             @touchend="middleTouchEnd"
+        >
           <div class="middle-l">
             <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd" :class="imgRotate">
                 <img class="image" :src="currentSong.image">
               </div>
-            </div>
-            <div class="playing-lyric-wrapper">
-              <div class="playing-lyric">{{playingLyric}}</div>
+              <div class="playing-lyric-wrapper">
+                <div class="playing-lyric">{{playingLyric}}</div>
+              </div>
             </div>
           </div>
           <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
@@ -106,7 +110,12 @@
   import Scroll from 'base/scroll/scroll'
 
   const transform = prefixStyle('transform')
+  const transitionDuration = prefixStyle('transitionDuration')
+
   export default {
+    created () {
+      this.touch = {}
+    },
     data() {
       return {
         radius: 32,
@@ -309,6 +318,11 @@
           if (this.playing) {
             this.currentLyric.play()
           }
+        }).catch(e => {
+          console.log(e)
+          this.currentLyric = null
+          this.playingLyric = ''
+          this.currentLineNum = 0
         })
       },
       handlerLyric({lineNum, txt}) {
@@ -320,6 +334,64 @@
           this.$refs.lyricList.scrollTo(0, 0, 1000)
         }
         this.playingLyric = txt
+      },
+      middleTouchStart(e) {
+        this.touch.initial = true
+        this.touch.moved = false
+        this.touch.startX = e.touches[0].pageX
+        this.touch.startY = e.touches[0].pageY
+      },
+      middleTouchMove(e) {
+        if (!this.touch.initial) {
+          return false
+        }
+        if (!this.touch.moved) {
+          this.touch.moved = true
+        }
+        let deltaX = e.touches[0].pageX - this.touch.startX
+        let deltaY = e.touches[0].pageY - this.touch.startY
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+          return false
+        }
+        let left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        let offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+        this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+        this.$refs.cdWrapper.style.opacity = 1 - this.touch.percent
+        this.$refs.cdWrapper.style[transitionDuration] = 0
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = 0
+      },
+      middleTouchEnd() {
+        if (!this.touch.moved) {
+          return false
+        }
+        let offsetWidth
+        let opacity
+        if (this.currentShow === 'cd') {
+          if (this.touch.percent > 0.1) {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+            this.currentShow = 'lyric'
+          } else {
+            offsetWidth = 0
+            opacity = 1
+          }
+        } else {
+          if (this.touch.percent < 0.9) {
+            offsetWidth = 0
+            opacity = 1
+            this.currentShow = 'cd'
+          } else {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+          }
+        }
+        const time = 300
+        this.$refs.cdWrapper.style.opacity = opacity
+        this.$refs.cdWrapper.style[transitionDuration] = `${time}ms`
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = `${time}ms`
+        this.touch.initiated = false
       },
       _pad(num) {
         return num > 9 ? num : '0' + num
